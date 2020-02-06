ea25da83d5d1d52ba949866fb1a5fa0743f3161e
Fadoua Amrou <faitamrou.90@gmail.com>
2020-02-06 11:39:47 -0500

    TipCalculator

---
+++ TipCalculator/TipCalculator/MainActivity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+
+namespace TipCalculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        EditText inputBill;
+        Button CalculateButton;
+        TextView outputTip;
+        TextView outputTotal;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.Main);
+
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            CalculateButton = FindViewById<Button>(Resource.Id.CalculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
+
+            CalculateButton.Click += OnClick;
+        }
+
+        public void OnClick(object sender, EventArgs e)
+        {
+            string s = inputBill.Text;
+
+            var bill = double.Parse(s);
+
+            double tip = bill * 0.15;
+            double total = bill + tip;
+
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = total.ToString();
+        }
+
+
+    }
+}
+
